58772c68a2d7a0e12a70e26cc141c8f59897d6b1
Rahul Singha <rex51@pop-os.localdomain>
2025-01-22 00:56:14 +0530

    test success

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Benefits from "./components/Benefits.jsx";
@@ -10,26 +8,26 @@ import FAQ from "./components/FAQ.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar fixed at top */}
-      <Navbar />
+	return (
+		<div className="flex flex-col min-h-screen">
+			{/* Navbar fixed at top */}
+			<Navbar />
 
-      {/* Main content (hero, benefits, etc.) */}
-      <main id="main-content" className="flex-1 relative pt-[4rem]">
-        {/* We add pt-[4rem] or so to offset the fixed navbar’s height. */}
-        <Hero />
-        <Benefits />
-        <Process />
-        <Pricing />
-        <Waitlist />
-        <FAQ />
-      </main>
+			{/* Main content (hero, benefits, etc.) */}
+			<main id="main-content" className="flex-1 relative pt-[4rem]">
+				{/* We add pt-[4rem] or so to offset the fixed navbar’s height. */}
+				<Hero />
+				<Benefits />
+				<Process />
+				<Pricing />
+				<Waitlist />
+				<FAQ />
+			</main>
 
-      {/* Footer at the bottom */}
-      <Footer />
-    </div>
-  );
+			{/* Footer at the bottom */}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
